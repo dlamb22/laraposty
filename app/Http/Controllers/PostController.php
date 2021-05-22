8f7acf27c58d1618e89b662dbf41c09e9631e9ee
Dominick <domlambwebdev@gmail.com>
2021-05-22 16:57:47 -0400

    post page, user page added

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,10 +7,20 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function __construct() {
+        $this->middleware(['auth'])->only('store', 'destroy');
+    }
+
     public function index() {
         // Get all the post to output to screen
         $posts = Post::with(['user', 'likes'])->orderBy('created_at', 'desc')->paginate(5);
         return view('posts.index')->with('posts', $posts);
+    }
+
+    public function show(Post $post) {
+        return view('posts.show', [
+            'post' => $post
+        ]);
     }
 
     public function store(Request $request) {
@@ -28,9 +38,8 @@ class PostController extends Controller
     }
 
     public function destroy(Post $post) {
-        if ($post->ownedBy(auth()->user())) {
-            //
-        }
+        
+        $this->authorize('delete', $post);
 
         $post->delete();
 
--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,16 @@ use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\PostLikeController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\UserPostController;
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
 
 Route::get('/member/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+
+Route::get('/users/{user:username}/posts', [UserPostController::class, 'index'])->name('users.posts');
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
@@ -25,6 +29,7 @@ Route::post('/logout', [LogoutController::class, 'index'])->name('logout');
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts');
 Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
 Route::post('/posts/{post}/likes', [PostLikeController::class, 'store'])->name('posts.likes');
